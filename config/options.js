4c67bf24066f5df2a56da89ed0b902da361fef6f
Makis Tracend <makis.tracend@gmail.com>
2016-02-10 18:20:24 -0800

    + option: serial

--- config/options.js
+++ config/options.js
@@ -3,7 +3,8 @@ var config = {
 	lock: 5000, // renew time lock
 	timeout: 5000, // client close timeout
 	store: "memory", // options: memory, redis, filesystem, custom
-	db: null // db where data are stored
+	db: null, // db where data are stored,
+	serial: false // process jobs in a serial order (not async)
 }
 
 module.exports = config;
