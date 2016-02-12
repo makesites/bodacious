0b8a5558b59b4146ec70d63e915ea47901f421a4
Makis Tracend <makis.tracend@gmail.com>
2016-02-11 18:50:35 -0800

    + example: redis

---
+++ examples/redis.js
@@ -0,0 +1,20 @@
+// The basic example using a redis store
+
+// lib
+var Queue = require("../index"); // in production: require('bodacious');
+
+// create queue
+var queue = new Queue('my_queue', { store: "redis" });
+
+// what to do with each job
+queue.process(function(job, done){
+	console.log("process job: ", job.id, job.data.timestamp);
+	done();
+});
+
+// adding queue items
+var interval = setInterval(function () {
+	var timestamp = Date.now();
+	console.log("new job: ", timestamp);
+	queue.add({ timestamp: timestamp });
+}, 100);
