eea102faca2f0c93f7788e61acf7f99dd41e1e7f
Makis Tracend <makis.tracend@gmail.com>
2016-02-13 23:49:00 -0800

    #6 + option: holdeFailed

--- config/options.js
+++ config/options.js
@@ -1,9 +1,10 @@
 // Defaults
 var config = {
 	autorun: true, // process new items in the queue automatically
-	db: null, // db where data are stored
 	concurrency: 1, // the number of concurrent jobs that can be processed
+	db: null, // db where data are stored
 	delay: 0, // how much (milliseconds) to delay between executing jobs
+	holdFailed: false,
 	lock: 5000, // renew time lock
 	retry: 5, // times to retry a failed job before erasing
 	store: "memory", // options: memory, redis, filesystem, custom
